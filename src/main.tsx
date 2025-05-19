eb61599c936f5724bd6ef54b4e9a70cfd3d3bd82
Danimexivasco <dcano9493@gmail.com>
2025-05-19 10:03:48 +0200

    Add MUI dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { render } from "preact";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
 import { App } from "./app.tsx";
 
-render(<App />, document.getElementById("app")!);
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark"
+  }
+});
+
+render(
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+  , document.getElementById("app")!);
